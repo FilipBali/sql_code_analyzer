2f9e365b66591d06c582068a9c7e9eeaf9f09f74
Filip Bali <14928466+FilipBali@users.noreply.github.com>
2023-04-06 00:41:45 +0200

    Creation of literal class

--- sql_code_analyzer/in_memory_representation/struct/literal.py
+++ sql_code_analyzer/in_memory_representation/struct/literal.py
@@ -2,5 +2,16 @@ from sql_code_analyzer.in_memory_representation.struct.base import Base
 
 
 class Literal(Base):
-    ...
 
+    def __init__(self,
+                 node,
+                 ):
+
+        self.value = node.name
+
+        self.is_int = node.is_int
+        self.is_number = node.is_number
+        self.is_star = node.is_star
+        self.is_string = node.is_string
+
+        self.args = node.args
