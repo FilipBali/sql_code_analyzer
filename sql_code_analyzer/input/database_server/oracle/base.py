55c1c68c14fd761158fe645494b743cfd6d0c062
Filip Bali <14928466+FilipBali@users.noreply.github.com>
2023-04-07 23:56:31 +0200

    Terminology updated

--- sql_code_analyzer/input/database_server/oracle/base.py
+++ sql_code_analyzer/input/database_server/oracle/base.py
@@ -10,6 +10,13 @@ if TYPE_CHECKING:
 
 
 def generate_dll(engine: create_engine, meta: MetaData):
+    """
+
+    :param engine:
+    :param meta:
+    :return:
+    """
+
     ddl = []
     for table in meta.sorted_tables:
         ddl.append(CreateTable(table).compile(engine))
@@ -22,6 +29,13 @@ def generate_dll(engine: create_engine, meta: MetaData):
 
 
 def oracle_handler(db_config):
+    """
+    Makes connection
+
+    :param db_config:
+    :return:
+    """
+
     engine, meta = make_connection(db_config)
     return generate_dll(engine=engine, meta=meta)
 
