f7a37c7f25248db41d677a6bfd201041e3c7dc39
Filip Bali <14928466+FilipBali@users.noreply.github.com>
2023-04-08 19:25:49 +0200

    Added code documentation

--- sql_code_analyzer/output/terminator/base.py
+++ sql_code_analyzer/output/terminator/base.py
@@ -2,7 +2,16 @@ from sql_code_analyzer.output import enums
 
 
 class Terminator:
+    """
+    A class that provides an interface for terminating a program.
+    """
 
     @staticmethod
-    def exit(exit_code: enums.ExitWith):
+    def exit(exit_code: enums.ExitWith) -> None:
+        """
+        A method that terminates a program based on the value of an argument.
+        :param exit_code: Exit value
+        :return: None
+        """
+
         exit(exit_code.value)
