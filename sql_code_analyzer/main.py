92f7bf8e7e6945a4255ea007ba7cca71bd4a4d64
Filip Bali <14928466+FilipBali@users.noreply.github.com>
2023-04-08 23:04:48 +0200

    Updated main (added termination Success)

--- sql_code_analyzer/main.py
+++ sql_code_analyzer/main.py
@@ -1,6 +1,8 @@
 from sql_code_analyzer.linter.linter import Linter
-
+from sql_code_analyzer.output import enums
+from sql_code_analyzer.output.terminator.base import Terminator
 
 if __name__ == "__main__":
 
     Linter()
+    Terminator.exit(enums.ExitWith.Success)
