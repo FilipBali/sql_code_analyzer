09f9889c62404aa3c55017a6d2f7708837f59c9c
Filip Bali <14928466+FilipBali@users.noreply.github.com>
2023-04-08 19:29:49 +0200

    Added code documentation

--- sql_code_analyzer/output/enums.py
+++ sql_code_analyzer/output/enums.py
@@ -2,18 +2,32 @@ import enum
 
 
 class OutputType(enum.Enum):
+    """
+    Enumerator for where the program output will be stored.
+    """
+
     Stdout = 1
     File = 2
     NoReport = 3
 
 
 class MessageType(enum.Enum):
+    """
+    Enumerator for message level/importance.
+    Used by Reporter class.
+    """
+
     Info = "\033[0m"
     Warning = '\033[1;33;48m'
     Error = "\033[31m"
 
 
 class ExitWith(enum.Enum):
+    """
+    Enumerator for different types of program termination.
+    Used by Terminator Class
+    """
+
     Success = 0
     InternalError = -1
     ArgumentError = -2
