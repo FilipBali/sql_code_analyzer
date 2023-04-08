e9e3062c9f23fc4d4a44ce61b6ea741bf5e946f7
Filip Bali <14928466+FilipBali@users.noreply.github.com>
2023-04-09 00:49:25 +0200

    Updated code documentation

--- sql_code_analyzer/in_memory_representation/struct/base.py
+++ sql_code_analyzer/in_memory_representation/struct/base.py
@@ -2,28 +2,22 @@
 # File name: base.py
 # Purpose: Base class contains common functionality for every class which represent some database object
 #
-# Key features:
-#     Base:
-#        Stores:
-#
-#        TODO:
-#
 #######################################
 from sql_code_analyzer.output.reporter.base import ProgramReporter
 
 
 class Base:
     """
-    TODO Description
+    Base class contains common functionality for every class which represent some database object
     """
 
     @staticmethod
     def check_if_exists(find_attr_val, struct, search_by_attr: str = None) -> bool:
         """
-        TODO Description
-        :param find_attr_val:
-        :param struct:
-        :param search_by_attr:
+        Dynamically checks if exists value in structure
+        :param find_attr_val: Value that will be searched for
+        :param struct: Structure where the search will take place
+        :param search_by_attr: Attribute name if necessary
         :return:
         """
         if isinstance(struct, list):
@@ -44,41 +38,14 @@ class Base:
             return False
 
     @staticmethod
-    def get_or_create(obj, struct, created_object):
-        """
-        TODO Description
-        :param obj:
-        :param struct:
-        :param created_object:
-        :return:
-        """
-        is_exists = obj.check_if_exists(obj, struct)
-
-        object_name = obj
-        if not isinstance(obj, str):
-            object_name = obj.name
-
-        if isinstance(struct, list):
-            if is_exists:
-                for item in struct:
-                    if item.name == object_name:
-                        return item
-            return created_object
-
-        elif isinstance(struct, dict):
-            if is_exists:
-                return struct[object_name]
-            return created_object
-
-    @staticmethod
     def get_instance_or_error(find_attr_val, find_in_struct, error_message: str, search_by_attr: str = None):
         """
-        TODO Description
-        :param find_attr_val:
-        :param find_in_struct:
-        :param error_message:
-        :param search_by_attr:
-        :return:
+        Return instance of object if exists or raise error
+        :param find_attr_val: Value that will be searched for
+        :param find_in_struct: Structure where the search will take place
+        :param error_message: Error message in case instance not exists
+        :param search_by_attr: Attribute name if necessary
+        :return: Instance or raise error
         """
         if isinstance(find_in_struct, list):
             for item in find_in_struct:
@@ -94,11 +61,11 @@ class Base:
     @staticmethod
     def get_instance_or_none(find_attr_val, find_in_struct, search_by_attr: str = None):
         """
-        TODO Description
-        :param find_attr_val:
-        :param find_in_struct:
-        :param search_by_attr:
-        :return:
+        Return instance of object if exists or return None
+        :param find_attr_val: Value that will be searched for
+        :param find_in_struct: Structure where the search will take place
+        :param search_by_attr: Attribute name if necessary
+        :return: Instance or None
         """
         if isinstance(find_in_struct, list):
             for item in find_in_struct:
