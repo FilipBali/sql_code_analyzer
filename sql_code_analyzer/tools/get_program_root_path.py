156d2b06b0b6cb2226de8286291c6d923d59c947
Filip Bali <14928466+FilipBali@users.noreply.github.com>
2023-04-08 19:31:13 +0200

    Added code documentation

--- sql_code_analyzer/tools/get_program_root_path.py
+++ sql_code_analyzer/tools/get_program_root_path.py
@@ -1,5 +1,10 @@
 import sys
 
 
-def get_program_root_path():
+def get_program_root_path() -> str:
+    """
+    Provides path to program root folder.
+    :return: Path to program root folder
+    """
+
     return sys.path[0]
