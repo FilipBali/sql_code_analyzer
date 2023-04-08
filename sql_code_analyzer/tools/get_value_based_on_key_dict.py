a0ef8341fd4427bdc34dd82bb1c4c7d0cb94590c
Filip Bali <14928466+FilipBali@users.noreply.github.com>
2023-04-08 19:32:14 +0200

    Added code documentation

--- sql_code_analyzer/tools/get_value_based_on_key_dict.py
+++ sql_code_analyzer/tools/get_value_based_on_key_dict.py
@@ -1,5 +1,12 @@
 
 def get_value_based_on_key_dict(d, v):
+    """
+    Returns key based on value if exists
+    :param d: Dictionary
+    :param v: Value
+    :return: Key or None
+    """
+
     for key, value in d.items():
         if v == value:
             return key
